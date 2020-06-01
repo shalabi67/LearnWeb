6b52cb639b3440c385706ab582ad6a3f6d229989
mohammad <shalabi67@hotmail.com>
2020-06-01 14:15:00 +0200

    learn mdc: using pure html

---
+++ MediaDesign/LearnMDC/codelab/my-solution/pure-html/auto-init/login.js
@@ -0,0 +1,8 @@
+/*
+mdc.textField.MDCTextField.attachTo(document.querySelector('.username'));
+mdc.textField.MDCTextField.attachTo(document.querySelector('.password'));
+
+mdc.ripple.MDCRipple.attachTo(document.querySelector('.cancel'));
+mdc.ripple.MDCRipple.attachTo(document.querySelector('.next'));
+ */
+mdc.autoInit();
---
+++ MediaDesign/LearnMDC/codelab/my-solution/pure-html/manual-init/login.js
@@ -0,0 +1,5 @@
+mdc.textField.MDCTextField.attachTo(document.querySelector('.username'));
+mdc.textField.MDCTextField.attachTo(document.querySelector('.password'));
+
+mdc.ripple.MDCRipple.attachTo(document.querySelector('.cancel'));
+mdc.ripple.MDCRipple.attachTo(document.querySelector('.next'));
