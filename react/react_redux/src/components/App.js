75b36cf81978a034fc77ae82dbdc0cd9c78de251
mohammadshalabi <sdfsd>
2020-06-26 15:46:23 +0200

    react-redux: adding routing, home, courses and about empty pages.

---
+++ react/react_redux/src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./home/HomePage";
+import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
+import PageNotFound from "./PageNotFound";
+import CoursesPage from "./courses/CoursesPage";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
+/*
+path="/about" is the one used in the link od HomePage to link to AboutPage.
+ */
